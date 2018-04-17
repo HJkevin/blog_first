7e4a77f26d22353f88cca666722579ce66ac32c6
Daisylemon <lmr1203624991@163.com>
2018-04-18 07:53:02 +0800

    2

--- blog_api/config/db_config.js
+++ blog_api/config/db_config.js
@@ -1,16 +1,16 @@
-module.exports = {
-    sql_config: {
-        host: "101.201.235.23",
-        user: "root",
-        password: "dig?fuckdang5",
-        port: 3306,
-        database: "nodetest"
+module.exports={
+    sql_config : {
+        host:'101.201.235.23',
+        port:'3306',
+        user:'root',
+        password:'1q2w3e',
+        database:'test'
     },
-    dev_db: {
-        host: "127.0.0.1",
-        user: "root",
-        password: "dig?fuckdang5",
-        port: 3306,
-        database: "nodetest"
+    dev_config : {
+        host:'127.0.0.1',
+        port:3306,
+        user:'root',
+        password:'1q2w3e',
+        database:'blog_sql'
     }
 }

a6b6ef2b9688874d4582019e20d91ca9f65d2b73
HJkevin <15652630926@163.com>
2018-04-19 08:26:38 +0800

    zb

--- blog_back/src/components/main/main.vue
+++ blog_back/src/components/main/main.vue
@@ -1,37 +1,13 @@
 <template>
-<<<<<<< HEAD
-  <div >
-   这里是主页
-</div>
-=======
   <div>
       <h2>
           欢迎来到个人博客后台管理系统
       </h2>
   </div>
->>>>>>> ee0599be9563a84014d0472a8ad05da536f96e7a
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
-  name: 'back_main',
-  data () {
-    return {
-		
-    }
-  },
-  methods:{
-    
-  }
-
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-=======
     name: 'back_main'
 }
 </script>
@@ -40,5 +16,4 @@ export default {
     h2{
         margin: 50px 20px;
     }
->>>>>>> ee0599be9563a84014d0472a8ad05da536f96e7a
 </style>

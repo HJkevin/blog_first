05c2f73188de529a059582b6a36f51c9c7c6149d
HJkevin <15652630926@163.com>
2018-04-19 08:41:59 +0800

    zb

--- blog_back/src/components/main/index.vue
+++ blog_back/src/components/main/index.vue
@@ -1,57 +1,19 @@
 <template>
-<<<<<<< HEAD
-  <div >
-    <headerC></headerC>
-    <div class="main">
-      <asideC class="left"></asideC>
-      <router-view class="right"></router-view>
-    </div>
-=======
   <div>
         <headerC></headerC>
         <div class="main">
             <asideC class="left"></asideC>
             <router-view class="right"></router-view>
         </div>
->>>>>>> ee0599be9563a84014d0472a8ad05da536f96e7a
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-
-  
 export default {
-  name: 'back_main',
-  data () {
-    return {
-		
-    }
-  },
-  methods:{
-    
-  }
-=======
-export default {
->>>>>>> ee0599be9563a84014d0472a8ad05da536f96e7a
 
 }
 </script>
 
-<<<<<<< HEAD
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.main{
-  overflow: hidden
-}
-.main .left{
-  float: left
-}
-.main .right{
-  float: left;
-
-}
-=======
 <style>
     .main{
         overflow: hidden
@@ -62,5 +24,4 @@ export default {
     .main .right{
         float: left;
     }
->>>>>>> ee0599be9563a84014d0472a8ad05da536f96e7a
 </style>

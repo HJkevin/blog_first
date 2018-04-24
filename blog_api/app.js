b8193503144099343d5ef433e5eee67626462be3
zhangyuepings <13699210284@163.com>
2018-04-24 08:18:21 +0800

    appjs

--- blog_api/app.js
+++ blog_api/app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser=require("body-parser")
-
+//加载ueditor 模块
+var ueditor = require("ueditor");
 
 var app = express();
 
@@ -29,12 +30,40 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // 接口
-
-
 var apiBackUser=require("./api/back/user.js")
 app.use('/api/back/user', apiBackUser);
 var apiBackClass=require("./api/back/class.js")
 app.use('/api/back/class', apiBackClass);
+
+// 文章
+var apiBackArticle=require("./api/back/article.js")
+app.use('/api/back/article', apiBackArticle);
+app.use('/api/back', apiBackArticle)
+
+//使用模块
+app.use("/api/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
+    // ueditor 客户发起上传图片请求
+    if (req.query.action === 'uploadimage') {
+        var foo = req.ueditor;
+
+        var imgname = req.ueditor.filename;
+
+        var img_url = '/ueditor/images/';
+        res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+        res.setHeader('Content-Type', 'text/html'); //IE8下载需要设置返回头尾text/html 不然json返回文件会被直接下载打开
+    }
+    //  客户端发起图片列表请求
+    else if (req.query.action === 'listimage') {
+        var dir_url = '/ueditor/images/';
+        res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
+    }
+    // 客户端发起其它请求
+    else {
+        console.log('config.json')
+        res.setHeader('Content-Type', 'application/json');
+        res.redirect('/ueditor/nodejs/config.js');
+    } 
+}));
 
 
 

490b39f00bceec334ebea071ea932893aacfc7bc
zhouxiaobo419 <528584768@qq.com>
2018-04-26 07:41:26 +0800

    code

--- blog_front/src/components/loding.vue
+++ blog_front/src/components/loding.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+    <div>
+      loding
+    </div>
 </template>
 <script>
 export default {
   data(){
       return{
-          
+
       }
   }
 }
--- blog_front/src/main.js
+++ blog_front/src/main.js
@@ -1,16 +1,23 @@
 
-// import aa from "./js/common.js"
-// console.log(aa)
-// import app from "./components/app.vue"
-// import Vue from " vue/dist/vue.js ";
 import Vue from "vue"
+import router from "./router"
+import store from "./store"
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
+import vueAxios from "vue-axios"
+import axios from "axios"
+Vue.use(vueAxios,axios)
+
+import Vuex from "vuex"
+Vue.use(Vuex)
+
 new Vue({
   el:"#root",
+  router,
+  store,
   template:"<app />",
   components: {
       app: resolve => require(['./components/app.vue'], resolve)
